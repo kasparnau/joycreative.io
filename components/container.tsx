a8295a02dd4d4b7d34b72e42db7ff6853edf7241
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-15 22:48:16 +0300

    feat: add optional className property to Container

--- components/container.tsx
+++ components/container.tsx
@@ -1,6 +1,14 @@
-export default function Container({ children }: { children: React.ReactNode }) {
+export default function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="max-w-5xl mx-auto px-6 md:px-12 xl:px-6 z-10">
+    <div
+      className={`max-w-5xl mx-auto px-6 md:px-12 xl:px-6 z-10 ${className && className}`}
+    >
       {children}
     </div>
   );
