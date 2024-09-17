4289fb1bf31c7a33aa33bf8017dd1ee468aebd67
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-17 06:06:15 +0300

    fix: remove z-index from container

--- components/container.tsx
+++ components/container.tsx
@@ -7,7 +7,7 @@ export default function Container({
 }) {
   return (
     <div
-      className={`max-w-7xl mx-auto px-6 md:px-12 xl:px-6 z-10 ${className || ""}`}
+      className={`max-w-7xl mx-auto px-6 md:px-12 xl:px-6 ${className || ""}`}
     >
       {children}
     </div>
