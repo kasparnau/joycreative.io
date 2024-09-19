96730e146fc579fbe0c397648649be3baa892aca
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-19 14:35:21 +0300

    feat: add Call to Action page

---
+++ app/(app)/(home)/components/call-to-action.tsx
@@ -0,0 +1,59 @@
+import Container from "@/components/container";
+import CtaGraphic from "@/public/assets/cta-graphic.svg";
+import Glows from "./glow-components";
+import Headings from "@/components/headings";
+import Image from "next/image";
+
+const CallToAction = () => {
+  return (
+    <div className="w-full flex items-center justify-center overflow-hidden relative mt-64">
+      {/* Layered Glow Effect */}
+      {/* CTA should be on z-2 to be over the blur*/}
+      <Glows.MiddleDim />
+      <Glows.MiddleBright />
+      <Glows.Bottom />
+
+      <Container className="w-full flex-col lg:flex-row flex items-center gap-x-32 gap-y-16">
+        {/* Call to Action */}
+        <div className="flex flex-col w-full gap-5 relative z-[2]">
+          <Headings.h1 className="text-nowrap">
+            Looking to <br />
+            collaborate?
+          </Headings.h1>
+          <Headings.h3 className="max-w-sm">
+            Want to bring your great idea to life? Drop a message — we'll help
+            you out.
+          </Headings.h3>
+          <div className="flex gap-2 mt-2">
+            <button className="rounded-card px-5 py-2 bg-foreground hover:bg-foreground/90 transition-colors text-muted w-fit text-md font-medium shadow-extra">
+              Let's work together
+            </button>
+            <button className="rounded-card px-5 py-2 bg-foreground/10 text-foreground w-fit text-md font-medium">
+              Learn more
+            </button>
+          </div>
+        </div>
+
+        {/* Graphic of game development */}
+        <div className="relative h-[600px] w-full">
+          <img
+            alt=""
+            src={CtaGraphic.src}
+            className="z-[1] min-w-[1200px] absolute left-0"
+          />
+        </div>
+        {/* <Image
+          alt=""
+          src={CtaGraphic}
+          className="w-[1200px] min-h-[600px] h-[600px] z-[1]"
+          style={{
+            // Fade out to the bottom
+            mask: "linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 50%)",
+          }}
+        /> */}
+      </Container>
+    </div>
+  );
+};
+
+export default CallToAction;
---
+++ app/(app)/(home)/components/hero.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import {
+  MotionValue,
+  easeOut,
+  inView,
+  motion,
+  useInView,
+  useMotionTemplate,
+  useMotionValueEvent,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+
+import Container from "@/components/container";
+import Glows from "./glow-components";
+import HandHoldingImg from "@/public/assets/holding-phone.png";
+import { useRef } from "react";
+
+const useOpacityTransform = (springScrollProgress: MotionValue) =>
+  useTransform(springScrollProgress, [0, 1], [1, 0]);
+
+interface HeroGlowProps {
+  springScrollProgress: MotionValue<any>;
+}
+
+const HeroGlow: React.FC<HeroGlowProps> = ({ springScrollProgress }) => {
+  // Smooth transform animation
+  const translateYTransform = useTransform(
+    springScrollProgress,
+    [0, 1],
+    ["0px", "300px"],
+  );
+  const translateY = useMotionTemplate`translateY(${translateYTransform})`;
+
+  // Smooth offset animation
+  const opacity = useOpacityTransform(springScrollProgress);
+
+  return (
+    <motion.div
+      className="h-full w-full pt-32 absolute bottom-0"
+      style={{
+        opacity: opacity,
+        transform: translateY,
+      }}
+      transition={{ easing: "easeOut" }}
+    >
+      <Glows.Bottom />
+    </motion.div>
+  );
+};
+
+interface HeroBorderProps {
+  scrollYProgress: MotionValue<any>;
+}
+
+const HeroBorder: React.FC<HeroBorderProps> = ({ scrollYProgress }) => {
+  // Smooth bottom border animation
+  const borderWithTransform = useTransform(scrollYProgress, [0, 0.7], [100, 0]);
+  const borderWidth = useMotionTemplate`${borderWithTransform}%`;
+
+  return (
+    <div className="w-full absolute bottom-0 flex justify-center">
+      <motion.div
+        className="h-[1px] bg-gradient-to-r from-transparent via-rose-500/30 to-transparent"
+        style={{ width: borderWidth }}
+      />
+    </div>
+  );
+};
+
+const HeroContent: React.FC<HeroGlowProps> = ({ springScrollProgress }) => {
+  const opacity = useOpacityTransform(springScrollProgress);
+
+  return (
+    <>
+      <Container className="w-full flex items-center justify-between">
+        <div className="w-full flex flex-col">
+          <motion.div
+            className="text-4xl sm:text-5xl md:text-7xl lg:text-[120px] font-bold text-nowrap"
+            transition={{ duration: 0.3 }}
+            initial={{ translateX: -1000, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+          >
+            We make games
+          </motion.div>
+          <motion.div
+            className="text-4xl sm:text-5xl md:text-7xl lg:text-[120px] font-bold text-nowrap relative lg:left-[33%]"
+            initial={{ translateX: 1000, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.1 }}
+          >
+            people love.
+          </motion.div>
+          <motion.div
+            className="text-md md:text-lg max-w-sm text-muted-foreground relative lg:left-[50%] mt-5"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.5 }}
+            variants={{
+              visible: { opacity: 1 },
+              hidden: { opacity: 0 },
+            }}
+          >
+            Independent game studio building immersive Metaverse experiences for
+            millions of players.
+          </motion.div>
+        </div>
+      </Container>
+      <motion.div
+        className=" absolute right-0 xl:right-36 -bottom-12"
+        initial={{ translateY: 50 }}
+        animate={{ translateY: 0 }}
+        transition={{ duration: 0.3 }}
+        style={{ opacity: opacity }}
+      >
+        <img
+          className="w-[400px] min-w-[400px]"
+          alt=""
+          src={HandHoldingImg.src}
+        />
+      </motion.div>
+    </>
+  );
+};
+
+const Hero = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // Scroll progress and scroll progress with spring for animation purposes
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start start", "end start"],
+  });
+
+  const springScrollProgress = useSpring(scrollYProgress, {
+    stiffness: 500,
+    damping: 100,
+  });
+
+  return (
+    <div
+      className="w-full flex flex-col relative overflow-hidden pt-32 min-h-screen"
+      ref={containerRef}
+    >
+      <HeroContent springScrollProgress={springScrollProgress} />
+      <HeroGlow springScrollProgress={springScrollProgress} />
+      <HeroBorder scrollYProgress={scrollYProgress} />
+    </div>
+  );
+};
+
+export default Hero;
