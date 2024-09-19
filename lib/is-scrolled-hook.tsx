e8f1ce49cb45bcb291fcc63b187d4b5389028235
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-19 14:55:59 +0300

    feat: add is-scrolled-hook

---
+++ lib/is-scrolled-hook.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const useIsScrolled = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsScrolled(scrollPosition > 0);
+  }, [scrollPosition]);
+
+  return isScrolled;
+};
+
+export default useIsScrolled;
