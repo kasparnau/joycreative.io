5c9602da62f9b04a9e5cc83c268b5e8f4da45650
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-19 14:27:10 +0300

    feat: enable lenis smooth scroll

--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,11 +1,38 @@
-import Container from "@/components/container";
+"use client";
+
+import { useEffect, useState } from "react";
+
+import CallToAction from "./components/call-to-action";
+import Hero from "./components/hero";
+import Lenis from "lenis";
 
 export default function Home() {
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    const resize = () => {
+      setDimension({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener("resize", resize);
+    requestAnimationFrame(raf);
+    resize();
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, []);
   return (
-    <main className="">
-      <Container>
-        <div>Hello world!</div>
-      </Container>
+    <main className="flex flex-col">
+      <Hero />
+      <CallToAction />
     </main>
   );
 }
