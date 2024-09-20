d774924918c79dca94d519f78a30490bb5e12aa4
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-20 10:49:03 +0300

    refactor: make easings pascalCase

--- lib/easings.ts
+++ lib/easings.ts
@@ -1,8 +1,8 @@
 // Custom easings I like to use for my animations.
 
-const Easings = {
+const easings = {
   easeInOutQuint: [0.83, 0, 0.17, 1],
 };
 
-export default Easings;
-export type Easing = (typeof Easings)[keyof typeof Easings];
+export default easings;
+export type easing = (typeof easings)[keyof typeof easings];
