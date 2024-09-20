1864bb13fa7a9ce05c10277cae71cba517f70c66
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-20 12:36:17 +0300

    feat: add Button component with variants

---
+++ components/button.tsx
@@ -0,0 +1,44 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg w-fit text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-foreground hover:bg-foreground/90 text-muted shadow-extra",
+        glass:
+          "bg-foreground/10 hover:opacity-80 text-foreground transition-opacity",
+      },
+      size: {
+        default: "px-5 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => {
+    return (
+      <button
+        className={twMerge(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+
+export { Button, buttonVariants };
