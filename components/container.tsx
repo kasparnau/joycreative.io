7cb5a843c3a8b741f070c52ca17a86cb4e4e7b06
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-21 16:53:23 +0300

    revert: revert last commit 'make website less wide'

--- components/container.tsx
+++ components/container.tsx
@@ -12,7 +12,7 @@ export default function Container({
   return (
     <div
       ref={ref}
-      className={`max-w-6xl mx-auto px-6 md:px-12 xl:px-6 ${className || ""}`}
+      className={`max-w-7xl mx-auto px-6 md:px-12 xl:px-6 ${className || ""}`}
     >
       {children}
     </div>
