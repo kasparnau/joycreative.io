007057887c87695415563242cd9cf90ea4ad08a3
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-21 16:51:24 +0300

    fix(headings): use twMerge to merge class prop

--- components/headings.tsx
+++ components/headings.tsx
@@ -2,19 +2,20 @@
 // They are divs and not actual CSS headings on purpose.
 
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 type Config = { [key: string]: string };
 export type Heading = { children?: ReactNode; className?: string };
 
 const config: Config = {
   h1: "text-5xl md:text-6xl font-medium text-foreground tracking-tight",
-  h2: "text-4xl font-medium text-foreground tracking-tight",
+  h2: "text-lg md:text-xl font-medium text-foreground",
   h3: "text-md md:text-lg text-muted-foreground leading-8",
 };
 
 const MakeBuildHeading = (style: string) => {
   function BuildHeading({ children, className }: Heading) {
-    return <div className={`${style} ${className || ""}`}>{children}</div>;
+    return <div className={twMerge(style, className)}>{children}</div>;
   }
 
   return BuildHeading;
