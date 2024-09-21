f6da3e377835f2439f435d50e71756a2461518a7
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-09-21 16:52:11 +0300

    style(root): make website less wide to improve readability

--- components/container.tsx
+++ components/container.tsx
@@ -1,13 +1,18 @@
+import { RefObject } from "react";
+
 export default function Container({
   children,
   className,
+  ref,
 }: {
   children: React.ReactNode;
   className?: string;
+  ref?: RefObject<HTMLDivElement>;
 }) {
   return (
     <div
-      className={`max-w-7xl mx-auto px-6 md:px-12 xl:px-6 ${className || ""}`}
+      ref={ref}
+      className={`max-w-6xl mx-auto px-6 md:px-12 xl:px-6 ${className || ""}`}
     >
       {children}
     </div>
