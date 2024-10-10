7e7adf7ec62fddd360bb0a30e9a35f8d2afa768b
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-10-10 19:23:19 +0300

    style(hero-phone): improve responsiveness and animation

--- app/(app)/(home)/components/hero/hero-phone.tsx
+++ app/(app)/(home)/components/hero/hero-phone.tsx
@@ -3,25 +3,33 @@ import { MotionValue, motion } from "framer-motion";
 import HandHoldingImg from "@/public/assets/hero/hero-phone.png";
 import Image from "next/image";
 import easings from "@/lib/easings";
+import { twMerge } from "tailwind-merge";
 
 const HeroPhone = ({ opacity }: { opacity: MotionValue<number> }) => {
   return (
-    <motion.div
-      className="h-[540px] sm:h-[590px] absolute -bottom-12 sm:-right-36"
-      initial={{ translateY: 50, opacity: 0 }}
-      animate={{ translateY: 0, opacity: 1 }}
-      transition={{ duration: 0.3, ease: easings.easeInOutQuint }}
-      style={{ opacity: opacity }}
+    <div
+      className={twMerge(
+        "absolute left-1/2 -translate-x-1/2 -bottom-12",
+        "sm:left-auto sm:right-0 lg:-right-36 sm:-translate-x-0",
+        "w-[267px] h-[393px] sm:w-[400px] sm:h-[590px]",
+      )}
     >
-      <Image
-        priority
-        className="object-cover h-[590px]"
-        width={400}
-        height={590}
-        alt=""
-        src={HandHoldingImg.src}
-      />
-    </motion.div>
+      <motion.div
+        className="w-full h-full"
+        initial={{ translateY: 50, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1 }}
+        transition={{ duration: 0.3, ease: easings.easeInOutQuint }}
+        style={{ opacity: opacity }}
+      >
+        <Image
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          src={HandHoldingImg.src}
+          priority
+          alt=""
+        />
+      </motion.div>
+    </div>
   );
 };
 
