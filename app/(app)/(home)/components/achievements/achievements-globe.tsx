b2047648add3df4a1e685881365be402fba982ff
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-10-10 19:07:51 +0300

    perf(achievements): make interactive earth a static image to prevent hitches

--- app/(app)/(home)/components/achievements/achievements-globe.tsx
+++ app/(app)/(home)/components/achievements/achievements-globe.tsx
@@ -1,75 +1,23 @@
 "use client";
 
-import { easings, useSpring } from "react-spring";
 import {
   motion,
   useMotionTemplate,
   useScroll,
   useTransform,
 } from "framer-motion";
-import { useEffect, useRef } from "react";
 
-import createGlobe from "cobe";
+import GlobeImg from "@/public/assets/achievements/globe.png";
+import Image from "next/image";
+import { easings } from "react-spring";
+import { useRef } from "react";
 
-export default function Cobe() {
-  // Interactive Globe from 5kB WebGL globe lib
-  // https://cobe.vercel.app/
-
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const pointerInteracting = useRef<number | null>(null);
-  const pointerInteractionMovement = useRef(0);
-  const [{ r }, api] = useSpring(() => ({
-    r: 4.3,
-    config: {
-      mass: 1,
-      tension: 280,
-      friction: 40,
-      precision: 0.001,
-    },
-  }));
-  useEffect(() => {
-    if (!canvasRef.current) return;
-
-    let width = 0;
-    const onResize = () =>
-      canvasRef.current && (width = canvasRef.current.offsetWidth);
-    window.addEventListener("resize", onResize);
-    onResize();
-    const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: width * 2,
-      height: width * 2,
-      phi: 5.2,
-      theta: 0.28,
-      dark: 1,
-      diffuse: 2,
-      mapSamples: 16000,
-      mapBrightness: 2,
-      baseColor: [0.7, 0.7, 0.7],
-      markerColor: [0.882, 0.114, 0.282],
-      glowColor: [0.0, 0.0, 0.0],
-      markers: [
-        { location: [60.1699, 24.9384], size: 0.05 }, //* HELSINKI
-      ],
-      onRender: (state) => {
-        // state.phi = r.get();
-        // state.width = width * 2;
-        // state.height = width * 2;
-      },
-    });
-    setTimeout(() => {
-      if (!canvasRef.current) return;
-      canvasRef.current.style.opacity = "1";
-    });
-    return () => {
-      globe.destroy();
-      window.removeEventListener("resize", onResize);
-    };
-  }, []);
+export default function Globe() {
+  const elementRef = useRef<HTMLDivElement | null>(null);
 
   // Scroll Animations
   const { scrollYProgress } = useScroll({
-    target: canvasRef,
+    target: elementRef,
     offset: ["start end", "end start"],
   });
 
@@ -83,56 +31,12 @@ export default function Cobe() {
 
   return (
     <motion.div
+      ref={elementRef}
       style={{
-        width: "100%",
-        maxWidth: 600,
-        aspectRatio: 1,
-        margin: "auto",
-        position: "relative",
         transform: motionTransform,
       }}
     >
-      <canvas
-        ref={canvasRef}
-        // onPointerDown={(e) => {
-        //   pointerInteracting.current =
-        //     e.clientX - pointerInteractionMovement.current;
-        //   canvasRef.current && (canvasRef.current.style.cursor = "grabbing");
-        // }}
-        // onPointerUp={() => {
-        //   pointerInteracting.current = null;
-        //   canvasRef.current && (canvasRef.current.style.cursor = "grab");
-        // }}
-        // onPointerOut={() => {
-        //   pointerInteracting.current = null;
-        //   canvasRef.current && (canvasRef.current.style.cursor = "grab");
-        // }}
-        // onMouseMove={(e) => {
-        //   if (pointerInteracting.current !== null) {
-        //     const delta = e.clientX - pointerInteracting.current;
-        //     pointerInteractionMovement.current = delta;
-        //     void api.start({
-        //       r: delta / 200,
-        //     });
-        //   }
-        // }}
-        // onTouchMove={(e) => {
-        //   if (pointerInteracting.current !== null && e.touches[0]) {
-        //     const delta = e.touches[0].clientX - pointerInteracting.current;
-        //     pointerInteractionMovement.current = delta;
-        //     void api.start({
-        //       r: delta / 100,
-        //     });
-        //   }
-        // }}
-        style={{
-          width: "100%",
-          height: "100%",
-          contain: "layout paint size",
-          // cursor: "auto",
-          userSelect: "none",
-        }}
-      />
+      <Image width={500} height={500} alt="" src={GlobeImg.src} />
     </motion.div>
   );
 }
