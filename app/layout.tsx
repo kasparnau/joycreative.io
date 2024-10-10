19b5de25da372d9cc0e5111413238af1a2515846
Kaspar <51763508+kasparnau@users.noreply.github.com>
2024-10-10 19:54:33 +0300

    feat(layout): add Open Graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,43 @@ import Header from "./(layout)/components/header/header";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import Providers from "./(layout)/components/providers";
+import { siteConfig } from "@/config/site";
 import { twMerge } from "tailwind-merge";
 
 export const metadata: Metadata = {
   title: "Joy Creative",
-  description:
-    "Joy Creative is an independent creative studio building digital experiences and pushing the boundaries of what's possible.",
-  icons: {
-    icon: "/logo.png",
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: ["UGC", "UEFN", "Game Development"],
+  authors: [
+    {
+      name: "Joy Creative",
+      url: "https://joycreative.io",
+    },
+  ],
+  creator: "Joy Creative",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.openGraphImage,
+        width: 1000,
+        height: 1000,
+        alt: siteConfig.name,
+      },
+    ],
   },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 const font = Outfit({ subsets: ["latin"] });
---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Joy Creative",
+  url: "https://joycreative.io",
+  openGraphImage: "https://joycreative.io/logo.png",
+  description:
+    "Independent studio connecting hundreds of thousands of people around the world through UGC games.",
+};
+
+export type SiteConfig = typeof siteConfig;
